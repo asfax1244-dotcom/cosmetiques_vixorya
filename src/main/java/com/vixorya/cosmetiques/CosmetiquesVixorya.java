c3805b44b2a263e9648db042b2929829cfc9b90e
asfax1244-dotcom <ton-email-github@exemple.com>
2025-08-21 21:39:36 +0200

    Premier commit

---
+++ src/main/java/com/vixorya/cosmetiques/CosmetiquesVixorya.java
@@ -0,0 +1,13 @@
+package com.vixorya.cosmetiques;
+
+import net.fabricmc.api.ModInitializer;
+
+public class CosmetiquesVixorya implements ModInitializer {
+    public static final String MODID = "cosmetiques_vixorya";
+
+    @Override
+    public void onInitialize() {
+        System.out.println("Cosmétiques Vixorya chargés !");
+        // Ici en vrai il faudrait enregistrer les entités (Krokmou) et items (ailes, queue)
+    }
+}
